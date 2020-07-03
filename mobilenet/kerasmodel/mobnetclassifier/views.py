7dfcae3a97e8635248a68c9f2e04fa811a1fd632
Sajan4072 <rai4072@gmail.com>
2020-07-03 20:46:19 +0545

    media upload

--- mobilenet/kerasmodel/mobnetclassifier/views.py
+++ mobilenet/kerasmodel/mobnetclassifier/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
-    context={'a':1}
+    context={}
     return render(request,'index.html',context)
     
+def classifier(request):
+    fileObj=request.FILES['fileP']
+    fs=FileSystemStorage()
+    path=fs.save(fileObj.name,fileObj)
+    path=fs.url(path)
+
+    
+
+    context={'path':path}
+    return render(request,'index.html',context)
 
